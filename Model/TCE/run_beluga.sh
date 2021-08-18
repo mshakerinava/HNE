3beae053b6a37839dfc8cbabbf8a7f30389b4e6b
Mehran Shakerinava <mehranshakerinava@gmail.com>
2021-08-17 21:38:05 -0400

    add beluga sbatch script

---
+++ Model/TCE/run_beluga.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=rrg-bengioy-ad
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mem=8G
+#SBATCH --time=12:00:00
+
+module load httpproxy
+source $HOME/venv_py38/bin/activate
+python main.py $@
+
