32184901bc7c3ef2f0d4d3252e7d6c0354c05ca4
Mehran Shakerinava <mehranshakerinava@gmail.com>
2021-08-17 21:53:55 -0400

    ignore data folders and make transform.sh receive arguments

--- Transform/transform.sh
+++ Transform/transform.sh
@@ -3,8 +3,8 @@
 # Note: Only 'R-GCN', 'HAN', 'MAGNN', and 'HGT' support attributed='True' or supervised='True'
 # Note: Only 'DBLP' and 'PubMed' contain node attributes.
 
-dataset='PubMed' # choose from 'DBLP', 'Yelp', 'Freebase', and 'PubMed'
-model='PTE' # choose from 'metapath2vec-ESim', 'PTE', 'HIN2Vec', 'AspEm', 'HEER', 'R-GCN', 'HAN', 'MAGNN', 'HGT', 'TransE', 'DistMult', 'ComplEx', and 'ConvE'
+dataset=$1 # choose from 'DBLP', 'Yelp', 'Freebase', and 'PubMed'
+model=$2 # choose from 'metapath2vec-ESim', 'PTE', 'HIN2Vec', 'AspEm', 'HEER', 'R-GCN', 'HAN', 'MAGNN', 'HGT', 'TransE', 'DistMult', 'ComplEx', and 'ConvE'
 attributed='False' # choose 'True' or 'False'
 supervised='False' # choose 'True' or 'False'
 
